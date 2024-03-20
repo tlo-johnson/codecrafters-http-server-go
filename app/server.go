dac3ca05d679d032a37fb8db6cee60a6e0ba2012
Tolu A <tolu@adesegha.ca>
2024-03-20 05:55:56 -0600

    attempt 2nd stage

--- app/server.go
+++ app/server.go
@@ -7,15 +7,26 @@ import (
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+  defer listener.Close()
 	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+  for {
+    connection, err := listener.Accept()
+    if err != nil {
+      fmt.Println("Error accepting connection: ", err.Error())
+      os.Exit(1)
+    }
+    go handleConnection(connection)
+  }
+}
+
+func handleConnection(connection net.Conn) {
+  defer connection.Close()
+
+  connection.Write([]byte("HTTP/1.1 200 OK\r\n"))
+  connection.Write([]byte("\r\n"))
 }
